9041dfe90c9833ce9cdd89327aaa58ccf04ca9cc
Venkata Krishna Rohit Sakala <rohit.sakala@suse.com>
2025-03-16 21:52:24 -0700

    add new version

--- package.sh
+++ package.sh
@@ -6,16 +6,28 @@ DEST_DIR="assets"
 INDEX_FILE="index.yaml"
 BASE_URL="assets"  # Replace with your actual base URL
 
-rm -r "$DEST_DIR"
+#rm -r "$DEST_DIR"
 # Ensure the assets directory exists
-mkdir -p "$DEST_DIR"
+#mkdir -p "$DEST_DIR"
 
 # Loop over all charts in the directory
 for chart in "$CHARTS_DIR"/*/*; do
-    if [ -d "$chart" ]; then
+    if [ -d "$chart" ]; then 
+        if [[ $chart != *"rancher-logging"* ]]; then
+            continue
+        fi
+        
         # Extract chart name from the directory structure: charts/{helmchartname}/{version}/
         CHART_NAME=$(basename "$(dirname "$chart")")
+        VERSION=$(basename "$chart")
 
+        if [[ "$(printf "%s\n%s" "105.2.1+up4.10.0" "$VERSION" | sort -V | head -n1)" != "105.2.1+up4.10.0" ]]; then
+            continue
+        fi
+
+        echo $CHART_NAME
+        echo $VERSION
+        
         # Create destination subdirectory
         CHART_DEST="$DEST_DIR/$CHART_NAME"
         mkdir -p "$CHART_DEST"
